1bc87f46e65f3d8261fa4097dcff7c9c767f7196
dongeon110 <dongeon110@gmail.com>
2023-01-26 11:33:41 +0900

    :art: [ADD] header, footer 파일 추가 및 기능 분리

--- src/main/java/com/example/book/springboot/web/IndexController.java
+++ src/main/java/com/example/book/springboot/web/IndexController.java
@@ -1,8 +1,10 @@
 package com.example.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
@@ -10,5 +12,10 @@ public class IndexController {
     public String index() {
         return "index"; // mustache starter 덕분에 앞의 경로는 src/main/resources/templates, 뒤로는 .mustache 가 붙음
         // 해당 경로는 src/main/resources/templates/index.mustache 로 전환되어 View Resolver 가 처리
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
