8f3e868615aebafda75d537711e212998527ffad
dongeon110 <dongeon110@gmail.com>
2023-01-27 09:51:00 +0900

    :sparkles: Feat: 게시글 수정기능 추가(fin)

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,10 +1,19 @@
 var main = {
     init : function () {
         var _this = this;
+        
+        // 저장 버튼
         $('#btn-save').on('click', function() {
             _this.save();
         });
+        
+        // 수정 버튼
+        $('#btn-update').on('click', function() {
+            _this.update();
+        })
     },
+    
+    // 저장
     save : function () {
         var data = {
             title: $('#title').val(),
@@ -22,6 +31,29 @@ var main = {
             alert('글이 등록되었습니다.');
             window.location.href = '/'; // 글 등록이 성공하면 메인페이지(/)로 이동
         }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    
+    // 수정
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function (error) {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     }
