f7461f45a48fbbce9e2f1f2802563531e0c4b16b
dongeon110 <dongeon110@gmail.com>
2023-01-30 16:47:46 +0900

    :white_check_mark: Test: 테스트 시큐리티 적용
    All Pass

--- src/main/java/com/example/book/springboot/Application.java
+++ src/main/java/com/example/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//@EnableJpaAuditing // JPA Auditing 활성화 // 테스트시 같이 스캔하게 되어 분리하기 위해 비활성화 JpaConfig로 이동
 @SpringBootApplication // Springboot의 자동 설정, 스프링 Bean 읽기와 생성 모두 자동
 // @SpringBootApplication 이 있는 위치부터 설정을 읽어가기 때문에 항상 프로젝트 최상단 위치
 public class Application {
