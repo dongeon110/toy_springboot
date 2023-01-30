20d545a3589cdeecd222c5e112ffefd937b57540
dongeon110 <dongeon110@gmail.com>
2023-01-30 16:47:46 +0900

    :white_check_mark: Test: 테스트 시큐리티 적용
    Posts_삭제된다 메서드 수정 필요
    그 외 테스트 통과

--- src/test/java/com/example/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/example/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.example.book.springboot.web;
 
+import com.example.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,7 +18,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class) // 테스트를 실행할 때, JUnit에 내장된 실행자 외에 다른 실행자 실행
 // 여기서는 SpringRunner라는 스프링 실행자를 사용
 // 스프링 부트 테스트와 JUnit 연결자 역할
-@WebMvcTest(controllers = HelloController.class) // 여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = { // WebMvcTest는 @Repository, @Service, @Component는 스캔대상이 아님, 따라서 SecurityConfig는 읽지만, CustomOAuth2UserService는 읽지 않아서 오류 발생
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
+// 여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션
 // 선언시 @Controller, @ControllerAdvice 등을 사용할 수 있음
 // 단, @Service, @Component, @Repository 등은 사용할 수 없음
 // 여기서는 컨트롤러만 사용하기 때문에 선언
@@ -23,6 +31,7 @@ public class HelloControllerTest {
     @Autowired // 스프링이 관리하는 빈을 주입받음
     private MockMvc mvc; // 웹 API 테스트 할때 사용, 스프링 MVC 테스트의 시작점, 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트 가능
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -32,6 +41,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // mvc.perform 결과 검증, String hello를 리턴하기 때문에 이 값이 맞는 지 검증
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
